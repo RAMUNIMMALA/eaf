595667721fd8107764bcf1000f0692822e62f46e
Ramu N <ramun@goggery.com>
2019-12-31 09:49:46 +0530

    uploaded new source

---
+++ src/main/java/base/BasePage.java
@@ -0,0 +1,11 @@
+package base;
+
+public abstract class BasePage implements IEAFbase {
+
+    //implementation method
+    public void loadBackgroundData(){
+
+    }
+
+    public abstract void verifyPageLoad();
+}
---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,9 @@
+package base;
+
+public abstract class BaseTest implements IEAFbase {
+
+    //implementation method
+    public void loadBackgroundData(){
+
+    }
+}
